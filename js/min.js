a989cc5c612e82ca2405cc9672ee5ff8a24ad3ba
Elena Beschastnaya <beschastnaya99elena@gmail.com>
2025-01-24 19:50:59 +0300

    menu js

--- js/min.js
+++ js/min.js
@@ -23,86 +23,7 @@
    });
  });
 
-// const cards = document.querySelectorAll('.card');
-// const tabletBreakpoint = 770; // Граница для мобильных
-//  // Граница для планшетов
 
-// function toggleAppear(element, add) {
-//     if (element) {
-//         if (add) {
-//             element.classList.add('appear');
-//         } else {
-//             element.classList.remove('appear');
-//         }
-//     }
-// }
-
-// function setupMobileListeners() {
-//     cards.forEach(card => {
-//         const butonAppearCard = card.querySelector('#butonAppearCard');
-
-//         card.addEventListener("click", () => {
-//             toggleAppear(butonAppearCard, undefined); // Используем toggle для click
-//         });
-//     });
-// }
-
-// function setupDesktopListeners() {
-//   cards.forEach(card => {
-//       const butonAppearCard = card.querySelector('#butonAppearCard');
-
-//       card.addEventListener("mouseenter", () => {
-//           toggleAppear(butonAppearCard, true);
-//       });
-
-//       card.addEventListener("mouseleave", () => {
-//            toggleAppear(butonAppearCard, false);
-//         });
-//     });
-// }
-
-// function removeAllListeners() {
-//     cards.forEach(card => {
-//         const butonAppearCard = card.querySelector('#butonAppearCard');
-
-//         card.removeEventListener('click', () => { });
-//         card.removeEventListener('mouseenter', () => { });
-//         card.removeEventListener('mouseleave', () => { });
-//        butonAppearCard.classList.remove('appear');
-//     });
-// }
-
-// function checkScreenSize() {
-//     removeAllListeners(); // Сначала убираем все слушатели
-//     // if (window.innerWidth <= mobileBreakpoint) {
-//     //     setupMobileListeners(); // Мобильные
-//     // } else 
-    
-//     if(window.innerWidth <= tabletBreakpoint) {
-//          setupMobileListeners(); // Планшеты - работают как мобильные
-//     }
-//     else {
-//         setupDesktopListeners(); // Компьютеры
-//     }
-// }
-
-// checkScreenSize();
-// window.addEventListener('resize', checkScreenSize);
-
-
-
-//     const nav = document.querySelector('#nav');
-// const navBtn = document.querySelector('#nav-btn');
-// const navBtnImg = document.querySelector('#nav-btn-img');
-
-// navBtn.onclick = () => {
-//     if (nav.classList.toggle('open')){
-//         navBtnImg.src ="imdge/close.svg";
-// }
-//     else {
-//         navBtnImg.src ="imdge/Menu.svg";
-// }
-// }
 
 
 
@@ -116,6 +37,21 @@ navLinks.forEach(link => {
       navBtnImgs.src ="imdge/Menu.svg";
   });
 });
+
+const nav = document.querySelector('#nav');
+const navBtn = document.querySelector('#nav-btn');
+const navBtnImg = document.querySelector('#nav-btn-img');
+
+navBtn.onclick = () => {
+    if (nav.classList.toggle('open')){
+        navBtnImg.src ="imdge/close.svg";
+}
+    else {
+        navBtnImg.src ="imdge/Menu.svg";
+}
+}
+
+
 
 
 
